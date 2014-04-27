7caf703615326a28208271e68e99f5479ec940ac
Andrew Swerlick <andrew.swerlick@gmail.com>
2014-04-27 17:56:11 -0400

    adding missing files

---
+++ SampleProject/Controllers/DinnerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using Swerl.Referee.Core;
+using Swerl.Referee.NerdDinnerSample.Models;
+using Swerl.Referee.NerdDinnerSample.Models.ViewModels;
+
+namespace Swerl.Referee.NerdDinnerSample.Controllers
+{
+    public class DinnerController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IAuthorizationService _service;
+
+        public DinnerController(ApplicationDbContext context, IAuthorizationService service)
+        {
+            _context = context;
+            _service = service;
+        }
+
+        public ActionResult Index()
+        {
+            var dinners = _context.Dinners;
+            return View(new DinnerIndexPageViewModel
+            {
+                Dinners = dinners.ToList(),
+                CanCreateDinner = _service.Authorize<DinnerController>(c=> c.Create(), User)
+            });
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
